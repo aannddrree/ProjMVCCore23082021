6778f866cf2796c6ad09ef5a9cb8e19ac2a9590b
unknown <aannddrree>
2021-08-23 12:02:25 -0300

    v0

---
+++ ProjMVCCore23082021/Data/ProjMVCCore23082021Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjMVCCore23082021.Models;
+
+namespace ProjMVCCore23082021.Data
+{
+    public class ProjMVCCore23082021Context : DbContext
+    {
+        public ProjMVCCore23082021Context (DbContextOptions<ProjMVCCore23082021Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjMVCCore23082021.Models.Client> Client { get; set; }
+    }
+}
---
+++ ProjMVCCore23082021/Models/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjMVCCore23082021.Models
+{
+    public class Client
+    {
+
+        #region Properties
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Tepephone { get; set; }
+
+        #endregion
+    }
+}
